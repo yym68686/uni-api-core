15787eb5c3d76305b51beb80d4a89b81ac35da89
yym68686 <yym68686@outlook.com>
2025-04-16 06:51:51 +0800

    update test file

--- test/test_base_api.py
+++ test/test_base_api.py
@@ -1,7 +1,5 @@
 from ..utils import BaseAPI
 
-import os
-
 # GOOGLE_AI_API_KEY = os.environ.get('GOOGLE_AI_API_KEY', None)
 
 # base_api = BaseAPI("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:streamGenerateContent?key=" + GOOGLE_AI_API_KEY)
@@ -9,9 +7,12 @@ import os
 # print(base_api.chat_url)
 
 base_api = BaseAPI("http://127.0.0.1:8000/v1")
-
 print(base_api.chat_url)
+
+base_api = BaseAPI("http://127.0.0.1:8000/v1/images/generations")
+print(base_api.image_url)
 
 """
 python -m core.test.test_base_api
+python -m aient.src.aient.core.test.test_base_api
 """
